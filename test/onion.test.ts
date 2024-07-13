6ead4edd7e4ebeacf2d1b52d8656f331e5e6db2c
land <ipjig@qq.com>
2024-07-13 22:38:51 +0800

    onion

--- test/onion.test.ts
+++ test/onion.test.ts
@@ -1,4 +1,4 @@
-import { oni } from "@/index";
+import { onion } from "@/index";
 import { expect, test } from "vitest";
 
 const end = async (x: number) => x;
@@ -14,17 +14,17 @@ const twice_next = async (x: number, next: () => Promise<number>) => {
 };
 test("null", async () => {
   //@ts-expect-error
-  const result = await oni<number, number>(null, end)(1);
+  const result = await onion<number, number>(null, end)(1);
   expect(result).toBe(1);
 });
 test("normal", async () => {
-  const result = await oni<number, number>([mid, mid, mid], end)(1);
+  const result = await onion<number, number>([mid, mid, mid], end)(1);
   expect(result).toBe(4);
 });
 
 test("twice_next", async () => {
   let count = 0;
-  const result = await oni<number, number>(
+  const result = await onion<number, number>(
     [twice_next, mid, mid],
     async (x) => {
       count++;
